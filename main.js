d5eba341175aedd8812f916e4b93636daf948f41
Solarin <solarinjohnson@gmail.com>
2023-02-22 21:20:56 +0100

    First commit

---
+++ main.js
@@ -0,0 +1,8 @@
+function menu(){
+    document.getElementById('menu').style.top = "100%"
+    document.getElementById('menu').style.top = "0"
+}
+
+function exit(){
+    document.getElementById('menu').style.top = "-100%"
+}
